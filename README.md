085dcafe2047250817c623d46cd67d7c90c5337e
Giorgio Zanni <g.zanni@hotmail.com>
2019-03-21 16:30:28 -0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Pomodoro clock web app built using react and accurateInterval.js from [Squeegy](https://gist.github.com/Squeegy/1d99b3cd81d610ac7351).
---
+++ precache-manifest.2166aaebd49d746edf0e2a3983911bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36c4b1f3a2d86819d04b",
+    "url": "/Pomodoro-Clock/static/css/main.78739a85.chunk.css"
+  },
+  {
+    "revision": "36c4b1f3a2d86819d04b",
+    "url": "/Pomodoro-Clock/static/js/main.15a0482a.chunk.js"
+  },
+  {
+    "revision": "0e2967b2ce3c0227ea16",
+    "url": "/Pomodoro-Clock/static/js/runtime~main.5703dadf.js"
+  },
+  {
+    "revision": "fb5438826a5e8784a3a3",
+    "url": "/Pomodoro-Clock/static/js/2.1419e559.chunk.js"
+  },
+  {
+    "revision": "ffaa300f1590c21108b5caadb26b12cc",
+    "url": "/Pomodoro-Clock/index.html"
+  }
+];
